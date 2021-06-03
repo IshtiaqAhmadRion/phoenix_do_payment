57fc39106ee03bbdbc5ec395af35b4c5f4d8a61a
IshtiaqAhmadRion <43097671+IshtiaqAhmadRion@users.noreply.github.com>
2021-06-04 03:19:49 +0600

    first Row

--- lib/containerDetails.dart
+++ lib/containerDetails.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:phoenix_do_payment/quantity.dart';
+//import 'package:phoenix_do_payment/quantity.dart';
 
 class ContainerDetails extends StatefulWidget {
   @override
@@ -9,29 +9,30 @@ class ContainerDetails extends StatefulWidget {
 class _ContainerDetailsState extends State<ContainerDetails> {
   String type;
   List size = ["20 GP", "40 HQ"];
+
+  final quantityKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    //var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
     //Total Column
     return Center(
       child: Column(
-        // mainAxisAlignment: MainAxisAlignment.start,
-        // mainAxisSize: MainAxisSize.max,
-        // crossAxisAlignment: CrossAxisAlignment.center,
-
         children: [
           //1st Row
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               //Row Padding
               Padding(
-                padding: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(30.0),
                 child: Container(
+                  width: screenWidth / 7,
                   decoration: buildBoxDecoration(),
-                  padding: EdgeInsets.only(left: 15, right: 15),
+                  padding: EdgeInsets.only(left: 10, right: 15),
                   child: DropdownButton(
                     hint: Text(
                       "Container Type",
+                      textAlign: TextAlign.center,
                     ),
                     value: type,
                     onChanged: (newValue) {
@@ -51,24 +52,66 @@ class _ContainerDetailsState extends State<ContainerDetails> {
                   ),
                 ),
               ),
-              //Quantity in first row
+              //Quantity
+              Container(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                width: screenWidth / 7,
+                child: Form(
+                  key: quantityKey,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value.length < 1) {
+                        return 'Please Enter Quantity';
+                      } else {
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Quantity',
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green[600])),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green[600])
+                          ),
+
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              // ignore: deprecated_member_use
+              Container(
+                width: screenWidth / 7,
+                // ignore: deprecated_member_use
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    padding:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+                    child: Text(
+                      "Submit",
+                      style: (TextStyle(fontSize: 20.00)),
+                    ),
+                    color: Color(0xff2D2D2D),
+                    textColor: Color(0xff0085CA),
+                    onPressed: () {
+                      quantityKey.currentState.validate();
+                    }),
+              ),
             ],
           ),
-          Container(
-            child: QuantityTextFromField(),
-          ),
-          // ignore: deprecated_member_use
-          
         ],
       ),
     );
   }
 
-//Extared Method
+//Extarct Method
   BoxDecoration buildBoxDecoration() {
     return BoxDecoration(
       border: Border.all(
-        color: Colors.purple,
+        color: Colors.green[600],
       ),
       borderRadius: BorderRadius.circular(10),
     );
